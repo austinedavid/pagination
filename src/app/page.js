a01d3b93a8bb870d62826787f7ab280723a0e272
david <austinedavid96@gmail.com>
2023-06-04 13:15:15 +0100

    running the link

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <p>main page</p>
+      <Link href="/post">to post</Link>
     </main>
   )
 }
